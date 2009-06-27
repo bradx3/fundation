a44711ff1a433dcb18fd2f0023a5fa40db262407
Brad Wilson <brad@lucky-dip.net>
2009-06-27 16:26:56 +1000

    added accounts scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :accounts
+
   map.resource :user_session
 #  map.connect ':controller/:action/:id'
 #  map.connect ':controller/:action/:id.:format'
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090627055506) do
+ActiveRecord::Schema.define(:version => 20090627061643) do
+
+  create_table "accounts", :force => true do |t|
+    t.string   "name"
+    t.integer  "balance_in_cents"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "login",                              :null => false
