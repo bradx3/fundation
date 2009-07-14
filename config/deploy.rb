f49b7168438fa50075f85423f4ea4c3fd054228f
Brad Wilson <brad@lucky-dip.net>
2009-07-14 15:57:49 +1000

    added cap, setup for prod

---
+++ config/deploy.rb
@@ -0,0 +1,24 @@
+set :application, "budgeto"
+set :repository,  "git@github.com:bradx3/offset.git"
+set :deploy_to, "/var/www/#{application}"
+set :scm, :git
+set :use_sudo, false
+
+host = "budgeto.lucky-dip.net"
+role :app, host
+role :web, host
+role :db, host, :primary => true
+
+namespace :deploy do
+
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+  
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+
+end
