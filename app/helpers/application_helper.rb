2265d21b927cd6e9ef0a5135168da26f4e4b21d8
Brad Wilson <brad@lucky-dip.net>
2009-08-12 20:19:02 +1000

    active tabs working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
   def currency(amount)
     number_to_currency(amount)
   end
+
+  def link_class(expected_controller_name)
+    return "active" if expected_controller_name == controller_name
+  end
 end
